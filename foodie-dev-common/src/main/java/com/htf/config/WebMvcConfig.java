012beeae6b80ab39f55129e8d77b8cfcd27183d6
sislick <1814610808@qq.com>
2020-04-10 15:52:50 +0800

    映射静态资源

--- foodie-dev-common/src/main/java/com/htf/config/WebMvcConfig.java
+++ foodie-dev-common/src/main/java/com/htf/config/WebMvcConfig.java
@@ -4,11 +4,24 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder){
         return builder.build();
+    }
+
+    /**
+     * 实现静态资源的映射
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/META-INF/resources/")//映射swagger2
+                .addResourceLocations("file:F:/imgs/");//映射本地静态资源
     }
 }
