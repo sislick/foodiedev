bec23e0895e9c3aab45e68f22cd1630196b43b5d
sislick <1814610808@qq.com>
2020-04-26 17:20:04 +0800

    redis 序列化问题

--- foodie-dev-controller/src/main/java/com/htf/controller/RedisController.java
+++ foodie-dev-controller/src/main/java/com/htf/controller/RedisController.java
@@ -1,6 +1,8 @@
 package com.htf.controller;
 
 import com.htf.base.BaseController;
+import com.htf.response.ResponseJSONResult;
+import com.htf.utils.RedisOperator;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,19 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class RedisController extends BaseController {
 
     @Autowired
-    private RedisTemplate redisTemplate;
+    private RedisOperator redisOperator;
 
     @GetMapping("/set")
     @ApiOperation(value = "set键值对", notes = "set键值对", httpMethod = "GET")
-    public Object set(String key, String value){
-        redisTemplate.opsForValue().set(key,value);
-        return "成功";
+    public ResponseJSONResult set(String key, String value){
+        redisOperator.set(key,value);
+        return ResponseJSONResult.create("成功");
     }
 
     @GetMapping("/get")
     @ApiOperation(value = "得到key的值", notes = "得到key的值", httpMethod = "GET")
-    public Object get(String key){
-        String value = (String)redisTemplate.opsForValue().get(key);
-        return value;
+    public ResponseJSONResult get(String key){
+        String value = (String)redisOperator.get(key);
+        return ResponseJSONResult.create(value);
     }
 }
